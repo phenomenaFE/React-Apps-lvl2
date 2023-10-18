b62b27bb850bfe999a954b5fc523844afee441b7
2p\halshorbagy <hazem.frontend@gmail.com>
2023-10-18 15:31:03 +0300

    sidebvar

--- share_me/shareme_frontend/shareme/src/components/Sidebar.jsx
+++ share_me/shareme_frontend/shareme/src/components/Sidebar.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
+import logo from "../assets/logo.png";
+import { categories } from "../utils/data";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const isNotActiveStyle =
+  "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
+const isActiveStyle =
+  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize";
+const Sidebar = ({ user, closeToggle }) => {
+  const handleCloseSideBar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-scroll ">
+      <div className="flex flex-col">
+        <Link
+          onClick={handleCloseSideBar}
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            onClick={handleCloseSideBar}
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-l">
+            Discover Categories
+          </h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSideBar}
+              key={category.name}
+            >
+              <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+              />
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
